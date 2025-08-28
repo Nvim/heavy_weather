2f7418688d8554ddfa0d4a241844f98f457c45e0
Naim <naimssj4@gmail.com>
2025-08-28 16:15:18 +0200

    add MultiPassRenderTarget for hardcoded MRT

--- heavy_weather/rendering/Backend/GL/GLRenderTarget.hpp
+++ heavy_weather/rendering/Backend/GL/GLRenderTarget.hpp
@@ -8,7 +8,7 @@ public:
   GLRenderTarget(std::pair<i32, i32> size);
   ~GLRenderTarget() override;
   u32 Handle() const override { return handle_; }
-  u32 ColorHandle() const override { return color_; }
+  u32 RGBHandle() const override { return color_; }
   u32 DepthHandle() const override { return depth_; }
 
 private:
@@ -17,4 +17,24 @@ private:
   u32 color_;
   u32 depth_;
 };
+
+class GLMultiPassRenderTarget : public MultiPassRenderTarget {
+public:
+  GLMultiPassRenderTarget(std::pair<i32, i32> size);
+  ~GLMultiPassRenderTarget() override;
+  u32 Handle() const override { return handle_; }
+  u32 RGBHandle() const override { return rgb_; }
+  u32 PositionHandle() const override { return position_; }
+  u32 NormalHandle() const override { return normal_; }
+  u32 DepthHandle() const override { return depth_; }
+
+private:
+  std::pair<i32, i32> size_;
+  u32 handle_;
+  u32 rgb_;
+  u32 position_;
+  u32 normal_;
+  u32 depth_;
+};
+
 } // namespace weather::graphics
--- heavy_weather/rendering/RenderTarget.hpp
+++ heavy_weather/rendering/RenderTarget.hpp
@@ -11,8 +11,21 @@ public:
   RenderTarget() = default;
   virtual ~RenderTarget() = default;
   virtual u32 Handle() const = 0;
-  virtual u32 ColorHandle() const = 0;
+  virtual u32 RGBHandle() const = 0;
   virtual u32 DepthHandle() const = 0;
+
+private:
+};
+
+class MultiPassRenderTarget : public RenderTarget {
+public:
+  MultiPassRenderTarget() = default;
+  ~MultiPassRenderTarget() override = default;
+  u32 Handle() const override = 0;
+  u32 RGBHandle() const override = 0;
+  u32 DepthHandle() const override = 0;
+  virtual u32 PositionHandle() const = 0;
+  virtual u32 NormalHandle() const = 0;
 
 private:
 };
